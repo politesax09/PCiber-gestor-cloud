254d38be804d6308ce60e14a2b36a9bcbf0bc581
Adrian <j.adrian.mazuecos@gmail.com>
2024-06-30 20:31:18 +0200

    Avances azure

--- azure/azure_create_vm.py
+++ azure/azure_create_vm.py
@@ -1,4 +1,3 @@
-from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
